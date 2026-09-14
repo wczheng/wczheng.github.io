9a16e13a2a56da348e049d8ab3993f10cf654809
zhengwc <zhengwc@zhengwcdeMacBook-Pro.local>
2026-09-14 23:32:39 +0800

    Replace emoji favicon with profile photo icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -132,7 +132,7 @@ export default function RootLayout({
   return (
     <html lang={runtimeI18n.defaultLocale} className="scroll-smooth" suppressHydrationWarning>
       <head>
-        <link rel="icon" href={config.site.favicon} type="image/svg+xml" />
+        <link rel="icon" href={config.site.favicon} type="image/png" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
